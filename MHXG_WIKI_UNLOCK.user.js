d9718203168c8fff36d0bb16a994601af90083b4
Shazi199 <shazi199@gmail.com>
2018-01-30 11:36:00 +0800

    add mhwg.org

--- MHXG_WIKI_UNLOCK.user.js
+++ MHXG_WIKI_UNLOCK.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         MHXG WIKI ReadOnly Unlock
 // @namespace    https://github.com/Shazi199/
-// @version      0.0.2
+// @version      0.0.3
 // @description  Unlock MHXG WIKI's right click and select.
 // @author       Shazi199
 // @license      MIT License
 // @match        http://wiki.mhxg.org/*
+// @match        http://mhwg.org/*
 // @grant        none
 // @require      http://code.jquery.com/jquery-1.11.0.min.js
 // ==/UserScript==
